3deea9dad60aa097dd7854c6644d8017fcf9c6bc
Sebastian Swoboda <73635276+SebastianSwoboda216@users.noreply.github.com>
2020-10-30 10:44:54 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,20 @@
 <?php
-echo "sebastian swoboda";
+
+$conn = new mysqli("mysql-sebastianswoboda.alwaysdata.net","217191","Dziwaczek1","");
+$sql = "SELECT * FROM pracownicy";
+$wynik = mysqli_query($conn, $sql);
+
+
+    echo('<table border="1">');
+    echo('<th>Imie</th><th>zarobki</th>');
+
+    while($wiersz=mysqli_fetch_assoc($wynik)){
+        echo('<tr>');
+        echo('<td>'.$wiersz['imie'].'</td>'.'<td>'.$wiersz['zarobki'].'</td>');
+        echo('</tr>');
+    }
+
+    echo('</table>');
+
+
 ?>
