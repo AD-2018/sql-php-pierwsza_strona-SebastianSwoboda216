d899b3780ea592985d201e1a02b21fa465301740
Sebastian Swoboda <73635276+SebastianSwoboda216@users.noreply.github.com>
2020-11-05 10:22:04 +0100

    Update index.php

--- index.php
+++ index.php
@@ -45,8 +45,19 @@ $result = mysqli_query($conn, $sql);
                 echo("<tr>"); 
                 echo("</tr>"); } 
                 echo('</table>'); 
+    
     $sql ="select * from pracownicy,organizacja where id_org=dzial and imie not like '%a' having zarobki<40"; 
-
+            echo("<h3>3</h3>"); 
+                $result = mysqli_query($conn, $sql);  
+                echo('<table border="1"'); 
+                echo ("<tr><th>imie</th><th>zarobki</th><th>data_urodzenia</th><th>dzial</th></tr>"); 
+     while($row=mysqli_fetch_assoc($result)){ 
+                echo("<tr>");     
+                echo("<tr>");     
+                echo ('<td>'.$row["imie"].'</td><td>'.$row["zarobki"].'</td><td>'.$row["data_urodzenia"].'</td><td>'.$row["dzial"].'</td>');    
+                echo("<tr>"); 
+                echo("</tr>"); } 
+                echo('</table>'); 
 
 ?>
 </body>
