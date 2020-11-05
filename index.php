b18a16472e54078dbeaa3329e06b80fbec5cf0b8
Sebastian Swoboda <73635276+SebastianSwoboda216@users.noreply.github.com>
2020-11-05 09:40:04 +0100

    Update index.php

--- index.php
+++ index.php
@@ -17,6 +17,14 @@ echo "Connected successfully";
     
 $sql = "SELECT * FROM pracownicy";
 $wynik = mysqli_query($conn, $sql);
+    
+echo ('<table border = "1" class = "moja_tabelka">');
+echo ("<tr><th>imie</th><th>zarobki</th><th>data_urodzenia</th><th>dzial</th></tr>");
+    while ($row = mysqli_fetch_assoc($result)) {
+    echo ('<tr>');
+    echo ('<td>'.$row["imie"].'</td><td>'.$row["zarobki"].'</td><td>'.$row["data_urodzenia"].'</td><td>'.$row["dzial"].'</td>');
+    echo ('</tr>');
+    }echo ('</table>');
 
 
     echo('<table border="1">');
