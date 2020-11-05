f271ee39bed073c05621c7d9e18babd7b1d4eb4b
Sebastian Swoboda <73635276+SebastianSwoboda216@users.noreply.github.com>
2020-11-05 08:52:41 +0100

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <a href="https://github.com/AD-2018/sql-php-pierwsza_strona-SebastianSwoboda216"</a>
+    <a href="https://github.com/AD-2018/sql-php-pierwsza_strona-SebastianSwoboda216">github</a>
     <h1>Sebastian Swoboda</h1>
 <?php
 
