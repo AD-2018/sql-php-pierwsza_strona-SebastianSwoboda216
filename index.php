0326fdddd50614f7dca99a88fad9e92aea134a52
Sebastian Swoboda <73635276+SebastianSwoboda216@users.noreply.github.com>
2020-11-05 10:31:13 +0100

    Update index.php

--- index.php
+++ index.php
@@ -21,7 +21,7 @@ $result = mysqli_query($conn, $sql);
         }echo ('</table>');
     
   $sql ="select avg(zarobki),nazwa_dzial from pracownicy,organizacja where id_org=dzial and imie not like '%a' group by dzial having avg(zarobki)<35"; 
-        echo("<h3>zadanie1</h3>"); 
+        echo("<h3>1</h3>"); 
             $result = mysqli_query($conn, $sql);  
                 echo('<table border="1"'); 
                 echo ("<tr><th>avg(zarobki)</th><th>nazwa_dzial</th></tr>"); 
@@ -47,7 +47,7 @@ $result = mysqli_query($conn, $sql);
                 echo('</table>'); 
     
     $sql ="select imie, zarobki from pracownicy,organizacja where id_org=dzial and imie not like '%a' group by imie having zarobki<40"; 
-            echo("<h3>zadanie3</h3>"); 
+            echo("<h3>3</h3>"); 
                 $result = mysqli_query($conn, $sql);  
                 echo('<table border="1" class="tabela"'); 
                 echo ("<tr><th>imie</th><th>zarobki</th></tr>"); 
@@ -58,6 +58,19 @@ $result = mysqli_query($conn, $sql);
                  echo("<tr>"); 
                  echo("</tr>"); } 
                  echo('</table>');  
+     
+    $sql ="select * from pracownicy,organizacja where id_org=dzial group by imie having dzial=2 or dzial=3"; 
+            echo("<h3>4</h3>"); 
+                $result = mysqli_query($conn, $sql);  
+                echo('<table border="1" class="tabela"'); 
+                echo ("<tr><th>id_pracownicy</th><th>imie</th><th>dzial</th><th>zarobki</th><th>data_urodzenia</th></tr>");
+        while($row=mysqli_fetch_assoc($result)){ 
+                echo("<tr>");     
+                echo("<tr>");     
+                echo("<td>".$row['id_pracownicy']."</td><td>".$row['imie']."</td><td>".$row['dzial']."</td><td>".$row['zarobki']."</td><td>".$row['data_urodzenia']."</td>");    
+                echo("<tr>"); 
+                echo("</tr>"); } 
+                echo('</table>'); 
 ?>
 </body>
 </html>
