842f257ef1e389ccadfc6e0ab5629f327e6fb36e
Sebastian Swoboda <73635276+SebastianSwoboda216@users.noreply.github.com>
2020-11-05 10:01:45 +0100

    Update index.php

--- index.php
+++ index.php
@@ -7,19 +7,23 @@
     <a href="https://github.com/AD-2018/sql-php-pierwsza_strona-SebastianSwoboda216">github</a>
     <h1>Sebastian Swoboda</h1>
 <?php
- 
 
-$conn= new mysqli("sebastianswoboda1.alwaysdata.net","217205","Dziwaczek123","sebastianswoboda1_uwagatojestbazadanych");   
-         $sql ="select * from pracownicy";
-$result = mysqli_query($conn, $sql);
-    
- echo ('<table border = "1" class = "moja_tabelka">');
-    echo ("<tr><th>imie</th><th>zarobki</th><th>data_urodzenia</th><th>dzial</th></tr>");
-        while ($row = mysqli_fetch_assoc($result)) {
-                echo ('<tr>');
-                echo ('<td>'.$row["imie"].'</td><td>'.$row["zarobki"].'</td><td>'.$row["data_urodzenia"].'</td><td>'.$row["dzial"].'</td>');
-                echo ('</tr>');
-        }echo ('</table>');
+$conn = new mysqli("mysql-sebastianswoboda1.alwaysdata.net","217205","Dziwaczek123","sebastianswoboda1_uwagatojestbazadanych");
+$sql = "SELECT * FROM pracownicy";
+$wynik = mysqli_query($conn, $sql);
+
+
+    echo('<table border="1">');
+    echo('<th>Imie</th><th>zarobki</th>');
+
+    while($wiersz=mysqli_fetch_assoc($wynik)){
+        echo('<tr>');
+        echo('<td>'.$wiersz['imie'].'</td>'.'<td>'.$wiersz['zarobki'].'</td>');
+        echo('</tr>');
+    }
+
+    echo('</table>');
+
 
 ?>
 </body>
