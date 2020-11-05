1d2d3af06260eb96dc36f9e0c8d8003a9c58e055
Sebastian Swoboda <73635276+SebastianSwoboda216@users.noreply.github.com>
2020-11-05 09:54:02 +0100

    Update index.php

--- index.php
+++ index.php
@@ -7,37 +7,24 @@
     <a href="https://github.com/AD-2018/sql-php-pierwsza_strona-SebastianSwoboda216">github</a>
     <h1>Sebastian Swoboda</h1>
 <?php
+$servername = mysql-"sebastianswoboda1.alwaysdata.net"; 
+$username = "217205"; 
+$password = "Dziwaczek123"; 
+$dbname = "sebastianswoboda1_uwagatojestbazadanych";  
 
-$conn = new mysqli("mysql-sebastianswoboda1.alwaysdata.net","217205","TeczeImotylki123","sebastianswoboda1_uwagatojestbazadanych");
-    if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
-}
-echo "Connected successfully";
-
-    
-$sql = "SELECT * FROM pracownicy";
-$wynik = mysqli_query($conn, $sql);
-    
-echo ('<table border = "1" class = "moja_tabelka">');
-echo ("<tr><th>imie</th><th>zarobki</th><th>data_urodzenia</th><th>dzial</th></tr>");
-    while ($row = mysqli_fetch_assoc($result)) {
-    echo ('<tr>');
-    echo ('<td>'.$row["imie"].'</td><td>'.$row["zarobki"].'</td><td>'.$row["data_urodzenia"].'</td><td>'.$row["dzial"].'</td>');
-    echo ('</tr>');
-    }echo ('</table>');
-
-
-    echo('<table border="1">');
-    echo('<tr><th>imie</th><th>zarobki</th><th>data_urodzenia</th><th>dzial</th></tr>');
-
-    while($wiersz=mysqli_fetch_assoc($wynik)){
-        echo('<tr>');
-        echo('<td>'.$wiersz['imie'].'</td>'.'<td>'.$wiersz['zarobki'].'</td>');
-        echo('</tr>');
-    }
-
-    echo('</table>');
-
+$conn= new mysqli($servername,$username,$password,$dbname);  
+$sql ="select * from pracownicy"; 
+    echo("<h3>tabelka</h3>"); 
+        $result = mysqli_query($conn, $sql);  
+            echo('<table border="1"'); 
+            echo ("<tr><th>id_pracownicy</th><th>imie</th><th>dzial</th><th>zarobki</th><th>data_urodzenia</th></tr>"); 
+            while($row=mysqli_fetch_assoc($result)){ 
+                echo("<tr>");     
+                echo("<tr>");     
+                echo("<td>".$row['id_pracownicy']."</td><td>".$row['imie']."</td><td>".$row['dzial']."</td><td>".$row['zarobki']."</td><td>".$row['data_urodzenia']."</td>");     
+                echo("<tr>"); 
+                echo("</tr>"); } 
+                echo('</table>'); 
 
 ?>
 </body>
