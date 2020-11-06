25e1db6728d1c7906bb38f37173727830ce2036f
Sebastian Swoboda <73635276+SebastianSwoboda216@users.noreply.github.com>
2020-11-06 10:13:40 +0100

    Update pracownicyorg.php

--- pracownicyorg.php
+++ pracownicyorg.php
@@ -1,3 +1,13 @@
-<?php
-echo "to pracownicyorg";
-?>
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <a href="https://github.com/AD-2018/sql-php-pierwsza_strona-SebastianSwoboda216">github</a>
+    <h1>Sebastian Swoboda</h1>
+    <div class="nav">
+    <a href="fagregujace.php">Funkcje Agregujące</a>
+</div>
+</body>
+</html>
