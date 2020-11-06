3b8e60d473e82c3e0389754d4e482c4f413a0cf5
Sebastian Swoboda <73635276+SebastianSwoboda216@users.noreply.github.com>
2020-11-07 00:02:32 +0100

    Update pracownicyorg.php

--- pracownicyorg.php
+++ pracownicyorg.php
@@ -4,9 +4,38 @@
 <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <div class="nav">
-    <a href="index.php">menu</a>
-
+<div class="nav">
+<a href="index.php">menu</a>
 </div>
+<?php
+require_once("connect.php");
+$sql ="select * from pracownicy,organizacja where id_org=dzial group by imie"; 
+echo("<h3>1</h3>"); 
+$result = mysqli_query($conn, $sql);
+if ( $result) {
+echo "<br>";
+} else {
+echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+}
+echo('<table border="1" class="tabela"'); 
+while($row=mysqli_fetch_assoc($result)){ 
+echo("<tr>");         
+echo("<td>".$row['imie']."</td><td>".$row['nazwa_dzial']."</td>");     
+echo("</tr>"); } 
+echo('</table>'); 
+$sql ="select * from pracownicy,organizacja where id_org=dzial group by imie having dzial=1 or dzial=4"; 
+echo("<h3>2</h3>"); 
+$result = mysqli_query($conn, $sql);
+if ( $result) {
+echo "<br>";
+} else {
+echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+}
+echo('<table border="1" class="tabela"'); 
+while($row=mysqli_fetch_assoc($result)){ 
+echo("<tr>");         
+echo("<td>".$row['imie']."</td><td>".$row['nazwa_dzial']."</td>");     
+echo("</tr>"); } 
+echo('</table>'); 
 </body>
 </html>
