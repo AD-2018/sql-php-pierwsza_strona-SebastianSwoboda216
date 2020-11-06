3bed35c73700ff721f6d53c586321c1437804a74
Sebastian Swoboda <73635276+SebastianSwoboda216@users.noreply.github.com>
2020-11-06 10:20:28 +0100

    Update fagregujace.php

--- fagregujace.php
+++ fagregujace.php
@@ -7,5 +7,18 @@
     <div class="nav">
     <a href="index.php">menu</a>
 </div>
+    <?php
+
+$conn = new mysqli("mysql-sebastianswoboda1.alwaysdata.net","217205","TeczeImotylki123","sebastianswoboda1_uwagatojestbazadanych");
+$sql ="select * from pracownicy";
+$result = mysqli_query($conn, $sql);
+    
+ echo ('<table border = "1">');
+    echo ("<tr><th>imie</th><th>zarobki</th><th>data_urodzenia</th><th>dzial</th></tr>");
+        while ($row = mysqli_fetch_assoc($result)) {
+                echo ('<tr>');
+                echo ('<td>'.$row["imie"].'</td><td>'.$row["zarobki"].'</td><td>'.$row["data_urodzenia"].'</td><td>'.$row["dzial"].'</td>');
+                echo ('</tr>');
+        }echo ('</table>');
 </body>
 </html>
