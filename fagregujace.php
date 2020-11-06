64039f2355ede29ab3d8ceee3caec06a9bce9cff
Sebastian Swoboda <73635276+SebastianSwoboda216@users.noreply.github.com>
2020-11-06 10:13:05 +0100

    Update fagregujace.php

--- fagregujace.php
+++ fagregujace.php
@@ -1,3 +1,13 @@
-<?php
-echo "to są funkcje agregujące";
-?>
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <a href="https://github.com/AD-2018/sql-php-pierwsza_strona-SebastianSwoboda216">github</a>
+    <h1>Sebastian Swoboda</h1>
+    <div class="nav">
+    <a href="pracownicyorg.php">Prac. Org.</a>
+</div>
+</body>
+</html>
