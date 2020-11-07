db2a08cb132936eabb3429cc7e8d595f5fa7ac02
Sebastian Swoboda <73635276+SebastianSwoboda216@users.noreply.github.com>
2020-11-07 01:22:11 +0100

    Update pracownicyorg.php

--- pracownicyorg.php
+++ pracownicyorg.php
@@ -10,7 +10,7 @@
 <?php
 
 require_once("connect.php");
-echo("<h2>Pracownicy  organizacja</h2>");
+echo("<h2>pracownicy  organizacja</h2>");
 $sql ="select * from pracownicy,organizacja where id_org=dzial group by imie"; 
 echo("<h3>1</h3>"); 
 $result = mysqli_query($conn, $sql);
@@ -27,6 +27,21 @@ echo("<td>".$row['imie']."</td><td>".$row['nazwa_dzial']."</td>");
 echo("</tr>"); } 
 echo('</table>'); 
 
+echo("<h3>2</h3>"); 
+$sql ="select * from pracownicy,organizacja where id_org=dzial group by imie having dzial=1 or dzial=4"; 
+$result = mysqli_query($conn, $sql);
+if ( $result) {
+echo "<br>";
+} else {
+echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+}
+echo('<table border="1" class="tabela"'); 
+echo ("<tr><th>imie</th><th>nazwa dzialu</th></tr>"); 
+while($row=mysqli_fetch_assoc($result)){ 
+echo("<tr>");         
+echo("<td>".$row['imie']."</td><td>".$row['nazwa_dzial']."</td>");     
+echo("</tr>"); } 
+echo('</table>'); 
    
 ?>
 </body>
