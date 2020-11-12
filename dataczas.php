5ceb74871eb859875a52818e5daa8686050885bf
Sebastian Swoboda <73635276+SebastianSwoboda216@users.noreply.github.com>
2020-11-12 09:02:45 +0100

    Update dataczas.php

--- dataczas.php
+++ dataczas.php
@@ -3,8 +3,11 @@
 <head>
 <link rel="stylesheet" href="style.css">
 </head>
-<body>
+    <div class="nav">
+    <a href="index.php">menu</a>
+</div>
 <?php
-?>
+require_once("connect.php");
+  ?>
 </body>
 </html>
