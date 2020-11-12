d41e02174a300e6a9aa537f385fd4fd937c52297
Sebastian Swoboda <73635276+SebastianSwoboda216@users.noreply.github.com>
2020-11-12 08:44:59 +0100

    Update fagregujace.php

--- fagregujace.php
+++ fagregujace.php
@@ -42,6 +42,23 @@ while($row=mysqli_fetch_assoc($result)){
 echo("<tr>");         
 echo("<td>".$row['Sum(zarobki)']."</td>");     
 echo("</tr>"); } 
+echo('</table>');
+    
+echo("<h3>3</h3>"); 
+$sql ="select Sum(zarobki) from pracownicy where imie not like '%a' and (dzial=2 or dzial=3)"; 
+echo ("<li>".$sql."</li><br><br>");
+$result = mysqli_query($conn, $sql);
+if ( $result) {
+echo "<br>";
+} else {
+echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+}
+echo('<table border="1" class="tabela"'); 
+echo ("<tr><th>Suma Zarobkow Mezczyzn z Dzialow 2 i 3</th></tr>"); 
+while($row=mysqli_fetch_assoc($result)){ 
+echo("<tr>");         
+echo("<td>".$row['Sum(zarobki)']."</td>");     
+echo("</tr>"); } 
 echo('</table>'); 
 ?>
 </body>
