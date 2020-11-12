66ee5f32e0326549a016897ce652bd4591989c1e
Sebastian Swoboda <73635276+SebastianSwoboda216@users.noreply.github.com>
2020-11-12 08:25:37 +0100

    Update fagregujace.php

--- fagregujace.php
+++ fagregujace.php
@@ -9,6 +9,38 @@
 </div>
 <?php
 require_once("connect.php");
+
+echo("<h3>1</h3>"); 
+$sql ="select Sum(zarobki) from pracownicy"; 
+$result = mysqli_query($conn, $sql);
+if ( $result) {
+echo "<br>";
+} else {
+echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+}
+echo('<table border="1" class="tabela"'); 
+echo ("<tr><th>Suma Zarobkow</th></tr>"); 
+while($row=mysqli_fetch_assoc($result)){ 
+echo("<tr>");         
+echo("<td>".$row['Sum(zarobki)']."</td>");     
+echo("</tr>"); } 
+echo('</table>'); 
+
+echo("<h3>2</h3>"); 
+$sql ="select Sum(zarobki) from pracownicy where imie like '%a'"; 
+$result = mysqli_query($conn, $sql);
+if ( $result) {
+echo "<br>";
+} else {
+echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+}
+echo('<table border="1" class="tabela"'); 
+echo ("<tr><th>Suma Zarobkow Kobiet</th></tr>"); 
+while($row=mysqli_fetch_assoc($result)){ 
+echo("<tr>");         
+echo("<td>".$row['Sum(zarobki)']."</td>");     
+echo("</tr>"); } 
+echo('</table>'); 
 ?>
 </body>
 </html>
