eb75b85e72ffdcf2b6a440e1b430ffe9b5852aa8
Sebastian Swoboda <73635276+SebastianSwoboda216@users.noreply.github.com>
2020-11-19 09:15:42 +0100

    Update form2.php

--- form2.php
+++ form2.php
@@ -2,7 +2,6 @@
 <head> Formularz osobowy </head>
 <title> Formularz osobowy </title>
 <body>
-<a href="index.php">menu</a>
 <form action="strona.php" method="POST">
 	<input type="text" name="firstname"></br>
 	<input type="text" name="lastname"></br>
@@ -10,6 +9,7 @@
 	<input type="text" name="phone"></br>
 	<input type="text" name="poscode"></br>
 	<input type="submit" value="wyślij do strona.php">
+<a href="index.php">menu</a>
 </form>
 </body>
 </html>
