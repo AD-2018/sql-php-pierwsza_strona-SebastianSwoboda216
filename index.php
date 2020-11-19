3c62de0a4ea1dc8269c77bbff0fe0dfe0250ac1a
Sebastian Swoboda <73635276+SebastianSwoboda216@users.noreply.github.com>
2020-11-19 08:58:26 +0100

    Update index.php

--- index.php
+++ index.php
@@ -4,6 +4,10 @@
 <link rel="stylesheet" href="style.css">
 </head>
 <body>
+    <form action="strona.php" method="POST">
+   <input type="text" name="firstname"></br>
+   <input type="submit" value="wyślij do strona.php">
+</form>
     <a href="https://github.com/AD-2018/sql-php-pierwsza_strona-SebastianSwoboda216">github</a>
     <h1>Sebastian Swoboda</h1>
     <div class="nav">
