81e6b078b6c08ff2ce30de5a9890cafc4251336c
Sebastian Swoboda <73635276+SebastianSwoboda216@users.noreply.github.com>
2020-11-20 10:52:05 +0100

    Update insert.php

--- insert.php
+++ insert.php
@@ -1,38 +1,18 @@
 <?php
-echo("jestes w insert.php <br>");
-echo $_POST['name'];
-echo $_POST['dzial'];
-echo $_POST['zarobki'];
-echo $_POST['data_urodzenia'];
-
 $servername = "mysql-sebastianswoboda1.alwaysdata.net";
 $username = "217205";
 $password = "TeczeImotylki123";
 $dbname = "sebastianswoboda1_uwagatojestbazadanych";
 
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = new mysqli ($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
-//definiujemy zapytanie $sql
-$sql = "INSERT INTO Pracownik (null, name, dzial,zarobki,data_urodzenia)
-	      VALUES (
-					null, 
-					$_POST['name'], 
-					$_POST['dzial'], 
-					$_POST['zarobki'],
-					$_POST['data_urodzenia']
-				";
+$sql = "INSERT INTO pracownicy (id_pracownicy, imie, dzial, zarobki, data_urodzenia) 
+       VALUES (null, 'Seba', 1, 76,'2002-08-18')";
 
-//wyświetlamy zapytanie $sql
-echo $sql;
-
-if ($conn->query($sql) === TRUE) {
-  echo "New record created successfully";
-} else {
-  echo "Error: " . $sql . "<br>" . $conn->error;
-}
+$conn->query($sql);
 
 $conn->close();
 ?>
