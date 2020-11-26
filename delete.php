e29996328c136d779a695b6b91ec1304fec9f1e6
Sebastian Swoboda <73635276+SebastianSwoboda216@users.noreply.github.com>
2020-11-26 08:35:26 +0100

    Update delete.php

--- delete.php
+++ delete.php
@@ -4,11 +4,6 @@ echo $_POST['id'];
 
 require_once("connect.php");
 
-$conn = new mysqli($servername, $username, $password, $dbname);
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
-}
-
 
 $sql = "DELETE FROM Pracownik WHERE id= $_POST['id'];";
 
