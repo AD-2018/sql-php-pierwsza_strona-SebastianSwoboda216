3827beb0077994722b22cf485d59d859bd2ca598
Sebastian Swoboda <73635276+SebastianSwoboda216@users.noreply.github.com>
2020-11-26 08:54:32 +0100

    Update insert.php

--- insert.php
+++ insert.php
@@ -1,12 +1,5 @@
 <?php
-$servername = "mysql-sebastianswoboda1.alwaysdata.net";
-$username = "217205";
-$password = "TeczeImotylki123";
-$dbname = "sebastianswoboda1_uwagatojestbazadanych";
-
-$conn = new mysqli ($servername, $username, $password, $dbname);
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
+require_once("connect.php");
 }
 
 $sql = "INSERT INTO pracownicy (id_pracownicy, imie, dzial, zarobki, data_urodzenia) 
