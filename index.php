62c77570719d4759ab311fa6376a330072fbf2f0
Sebastian Swoboda <73635276+SebastianSwoboda216@users.noreply.github.com>
2020-11-26 09:16:39 +0100

    Update index.php

--- index.php
+++ index.php
@@ -15,6 +15,28 @@
     <a href="form2.php">Formularz osobowy</a>
     <a href="strona.php">Strona</a>
     <a href="danedobazy.html">Usuwanie dodawanie</a>
+        
+<?php
+require_once("connect.php");
+$sql = "SELECT * FROM pracownicy, organizacja WHERE id_org = dzial";
+if ($conn->connect_error) {
+die("Connection failed: " . $conn->connect_error);
+}
+$result = mysqli_query($conn, $sql);
+if ( $result) {
+echo "<br>";
+} else {
+echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+}
+echo("<table border='1'>");
+echo("<th>ID</th><th>Imie</th><th>Zarobki</th><th>Data Urodzenia</th><th>Dzial</th><th>Nazwa dzialu</th>");
+while($row = mysqli_fetch_assoc($result)) {
+echo("<tr>");
+echo("<td>".$row['id_pracownicy']."</td><td>".$row['imie']."</td><td>".$row['zarobki']."</td><td>".$row['data_urodzenia']."</td><td>".$row['dzial']."</td><td>".$row['nazwa_dzial']."</td>");
+echo("</tr>");
+};
+echo("</table>");
+?>
 </div>
 </body>
 </html>
