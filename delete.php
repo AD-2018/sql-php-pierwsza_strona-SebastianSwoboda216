60c4fac306a017e7f4c15f75d3daa621aa06a9d7
Sebastian Swoboda <73635276+SebastianSwoboda216@users.noreply.github.com>
2020-11-26 09:18:57 +0100

    Update delete.php

--- delete.php
+++ delete.php
@@ -1,8 +1,7 @@
 <?php
 require_once("connect.php");
 
-$sql = "INSERT INTO pracownicy (id_pracownicy,imie, dzial, zarobki,data_urodzenia) 
-       VALUES (null,".'"'.$_POST['imie'].'"'.','.$_POST['dzial'].','.$_POST['zarobki'].','.'"'.$_POST['data_urodzenia'].'"'.')';
+
 
 
 if ($conn->query($sql) === TRUE) {
