5352775473271e0af6da4e513c032ccc25e7df74
Sebastian Swoboda <73635276+SebastianSwoboda216@users.noreply.github.com>
2020-11-26 09:03:45 +0100

    Update insert.php

--- insert.php
+++ insert.php
@@ -2,7 +2,7 @@
 require_once("connect.php");
 
  $sql = "INSERT INTO pracownicy (id_pracownicy,imie, dzial, zarobki,data_urodzenia)
-WARTOŚCI (null",. ""'.  $_POST['imie']. ""'. ',',.  $_POST['dzial']. ',',.  $_POST['zarobki']. ',',.  $_POST["data_urodzenia"].')';
+WARTOŚCI (null",. ""'.  $_POST['imie']. ""'. ',',.  $_POST['dzial']. ',',.  $_POST['zarobki']. ',',.  $_POST['data_urodzenia'].')';
 
 $conn->query($sql);
 
