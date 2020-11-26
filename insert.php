35027afc1fdd776bb61176111884928cf6b297fb
Sebastian Swoboda <73635276+SebastianSwoboda216@users.noreply.github.com>
2020-11-26 09:21:43 +0100

    Update insert.php

--- insert.php
+++ insert.php
@@ -4,7 +4,12 @@ require_once("connect.php");
 $sql = "INSERT INTO pracownicy (id_pracownicy,imie, dzial, zarobki,data_urodzenia) 
        VALUES (null,".'"'.$_POST['imie'].'"'.','.$_POST['dzial'].','.$_POST['zarobki'].','.'"'.$_POST['data_urodzenia'].'"'.')';
 
-$conn->query($sql);
+
+if ($conn->query($sql) === TRUE) {
+  echo $sql;
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
 
 $conn->close();
 ?>
