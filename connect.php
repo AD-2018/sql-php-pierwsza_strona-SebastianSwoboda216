d934c4fdc248c92487430b57da91e2b1d47d06f1
Sebastian Swoboda <73635276+SebastianSwoboda216@users.noreply.github.com>
2020-12-03 08:47:11 +0100

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,8 +1,8 @@
 <?php
-$servername = "mysql-sebastianswoboda1.alwaysdata.net";
-$username = "217205";
-$password = "Dziwaczek1";
-$dbname = "sebastianswoboda1_uwagatojestbazadanych";
+$servername = "mysql-aaaaaaa.alwaysdata.net";
+$username = "aaaaaaa";
+$password = "SupcioChrupcio123";
+$dbname = "aaaaaaa_tojestbazadanych";
 $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
