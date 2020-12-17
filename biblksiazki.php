eb6c0bfea4347984ea16a079a57dec162de2eac7
Sebastian Swoboda <73635276+SebastianSwoboda216@users.noreply.github.com>
2020-12-17 08:53:33 +0100

    Update biblksiazki.php

--- biblksiazki.php
+++ biblksiazki.php
@@ -4,8 +4,6 @@
 <link rel="stylesheet" href="style.css">
 </head>
 <body>
-<div class = "nav">
-</div>
 <?php   
 require_once("connect.php");
 $sql = "select (`biblAutor_biblTytul`.id), autor, tytul, biblWypoz from biblAutor_biblTytul,biblAutor,biblTytul where biblAutor.id=biblAutor_id and biblTytul.id=biblTytul_id";
