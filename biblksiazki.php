f1a2e1c19222b4f8a580c6de6188757f303a6fed
Sebastian Swoboda <73635276+SebastianSwoboda216@users.noreply.github.com>
2020-12-17 08:44:54 +0100

    Update biblksiazki.php

--- biblksiazki.php
+++ biblksiazki.php
@@ -12,7 +12,6 @@ echo("<h3> Biblioteka </h3>");
 $sql = "SELECT * FROM bibl_book";
 echo ("<li>".$sql."</li><br><br>");
 $result = mysqli_query($conn, $sql);
-echo ('<table border = "1" class = "moja_tabelka">');
 echo ("<tr><th>ID</th><th>Pisarz</th><th>Powiesc</th><th>Wypozyczenia</th></tr>");
 while ($row = mysqli_fetch_assoc($result)) {
 echo ('<tr>');
@@ -20,11 +19,10 @@ echo ('<td>'.$row["id_book"].'</td><td>'.$row["pisarz"].'</td><td>'.$row["powies
 echo ('</tr>'); }
 echo ('</table>');
     
-echo("<h3> AUTORZY </h3>");
+echo("<h3> Autor </h3>");
 $sql = "SELECT * FROM bibl_autor";
 echo ("<li>".$sql."</li><br><br>");
 $result = mysqli_query($conn, $sql);
-echo ('<table border = "1" class = "moja_tabelka">');
 echo ("<tr><th>ID</th><th>Autor</th></tr>");
 while ($row = mysqli_fetch_assoc($result)) {
 echo ('<tr>');
@@ -32,11 +30,10 @@ echo ('<td>'.$row["id_autor"].'</td><td>'.$row["autor"].'</td>');
 echo ('</tr>'); }
 echo ('</table>');
     
-echo("<h3> TYTULY </h3>");
+echo("<h3> Tytuł </h3>");
 $sql = "SELECT * FROM bibl_tytul";
 echo ("<li>".$sql."</li><br><br>");
 $result = mysqli_query($conn, $sql);
-echo ('<table border = "1" class = "moja_tabelka">');
 echo ("<tr><th>ID</th><th>ksiazka</th></tr>");
 while ($row = mysqli_fetch_assoc($result)) {
 echo ('<tr>');
