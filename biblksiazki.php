f9d9d37ec12a4af4f2a81fcda568821ecd8afebd
Sebastian Swoboda <73635276+SebastianSwoboda216@users.noreply.github.com>
2020-12-17 08:51:57 +0100

    Update biblksiazki.php

--- biblksiazki.php
+++ biblksiazki.php
@@ -8,8 +8,19 @@
 </div>
 <?php   
 require_once("connect.php");
-
-  
+$sql = "select (`biblAutor_biblTytul`.id), autor, tytul, biblWypoz from biblAutor_biblTytul,biblAutor,biblTytul where biblAutor.id=biblAutor_id and biblTytul.id=biblTytul_id
+if ($conn->connect_error) {
+die("Connection failed: " . $conn->connect_error);
+}
+$result = mysqli_query($conn, $sql);
+if ( $result) {
+} else {
+echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+}
+echo("<table border='1' class='tabela'>");
+while($row = mysqli_fetch_assoc($result)) {
+echo("<tr>");
+echo("<td>".$row['biblWypoz']."</td><td>".$row['autor']."</td><td>".$row['tytul']."</td>");  
 ?>
 </body>
 </html>
