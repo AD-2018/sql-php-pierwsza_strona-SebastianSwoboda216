94c4403a1406ce1ed59a676ab881e65993a3c189
Sebastian Swoboda <73635276+SebastianSwoboda216@users.noreply.github.com>
2021-01-28 14:40:34 +0100

    Update delete.php

--- pracownicy/delete.php
+++ pracownicy/delete.php
@@ -7,7 +7,7 @@ echo "<br>";
 $sql = "DELETE FROM pracownicy WHERE id_pracownicy=".$_POST['id'];
 
 if ($conn->query($sql) === TRUE) {
-  header('Location: https://sebastian-swoboda.herokuapp.com/danedobazy.php');
+  header('Location: https://sebastian-swoboda.herokuapp.com//danedobazy.php');
 } else {
   echo "Error: " . $sql . "<br>" . $conn->error;
 }
