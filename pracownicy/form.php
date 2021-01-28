df545a040dcbc4b01572761adc3927313e7b1583
SebastianSwoboda216 <sebastian.swoboda83@gmail.com>
2021-01-28 14:16:55 +0100

    test

---
+++ pracownicy/form.php
@@ -0,0 +1,11 @@
+<html>
+<head> Formularz </head>
+<title> Formularz </title>
+<body>
+<form action="strona.php" method="POST">
+   <input type="text" name="firstname"></br>
+   <input type="submit" value="wyślij do strona.php">
+</form>
+   <a href="index.php">menu</a>
+</body>
+</html>
---
+++ pracownicy/form2.php
@@ -0,0 +1,15 @@
+<html>
+<head> Formularz osobowy </head>
+<title> Formularz osobowy </title>
+<body>
+<form action="strona.php" method="POST">
+	<input type="text" name="firstname"></br>
+	<input type="text" name="lastname"></br>
+	<input type="text" name="city"></br>
+	<input type="text" name="phone"></br>
+	<input type="text" name="poscode"></br>
+	<input type="submit" value="wyślij do strona.php">
+<a href="index.php">menu</a>
+</form>
+</body>
+</html>
