6dd3d25be611556f57254d6e0104ca7d7719fb5f
Sebastian Swoboda <73635276+SebastianSwoboda216@users.noreply.github.com>
2021-01-28 14:30:36 +0100

    Update pracownicy.php

--- pracownicy/pracownicy.php
+++ pracownicy/pracownicy.php
@@ -2,18 +2,18 @@
 <!DOCTYPE html>
 <html>
 <head>
-<link rel="stylesheet" href="style.css">
+<link rel="stylesheet" href="../style.css">
 </head>
 <body>
     <div class="nav">
-    <a href="index.php">menu</a>
+    <a href="../index.php">menu</a>
     <a href="pracownicyorg.php">Pracownicy Organizacja.</a>
     <a href="fagregujace.php">Funkcje Agregujące</a>
     <a href="dataczas.php">Data i czas</a>
 </div>
 <?php
 
-require_once("connect.php");
+require_once("../connect.php");
     
 $sql ="select imie,dzial from pracownicy,organizacja where id_org=dzial and dzial=2 group by imie"; 
 echo ("<li>".$sql."</li><br><br>");
