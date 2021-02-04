187d54773c8c709fd0822e62f08a568137e61b01
SebastianSwoboda216 <sebastian.swoboda83@gmail.com>
2021-02-04 19:25:04 +0100

    sfs

--- biblioteka/biblksiazki.php
+++ biblioteka/biblksiazki.php
@@ -22,6 +22,10 @@
 <input type="submit" value="dodaj">
 </form>
 </div>
+</div>
+<div>
+<?php 
+require_once("../connect.php"); 
 $sql = "select ('biblAutor_biblTytul'.id) as ID_TAB, autor, tytul, biblWypoz from biblAutor_biblTytul,biblAutor,biblTytul where biblAutor.id=biblAutor_id and biblTytul.id=biblTytul_id order by autor,ID_TAB asc";
 $result = mysqli_query($conn, $sql);
 if ( $result) {
@@ -36,10 +40,6 @@ echo "</option>";
 };
 echo('</select>');
 echo ('<br>');
-</div>
-<div>
-<?php 
-require_once("../connect.php"); 
 echo("<h1>Tytuł</h1>"); 
 $sql = "select * from biblTytul";
 echo("<li>".$sql);
