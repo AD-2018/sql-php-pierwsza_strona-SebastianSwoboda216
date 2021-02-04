3663bc3b292a85af7e55fd3ef80ea5fee5a14835
SebastianSwoboda216 <sebastian.swoboda83@gmail.com>
2021-02-04 14:29:29 +0100

    gh

---
+++ biblioteka/autor.php
@@ -0,0 +1,18 @@
+<html>
+<head>
+<link rel="stylesheet" href="../style.css">
+</head>
+<body>
+<?php
+require_once("../connect.php");
+$sql = "INSERT INTO biblTytul (id,tytul) 
+       VALUES (null,".'"'.$_POST['tytul'].'"'.')';
+if ($conn->query($sql) === TRUE) {
+  header('Location: https://sebastian-swoboda.herokuapp.com/biblioteka/biblksiazki.php');
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
+$conn->close();
+?>
+</body>
+</html>
