27f2619ea6c31f922ac5f886d723c211c874abfa
Sebastian Swoboda <73635276+SebastianSwoboda216@users.noreply.github.com>
2021-02-04 13:30:56 +0100

    Update biblksiazki.php

--- biblioteka/biblksiazki.php
+++ biblioteka/biblksiazki.php
@@ -6,15 +6,15 @@
 <body>
 <?php   
 require_once("connect.php");
-$sql = "select (`biblAutor_biblTytul`.id), autor, tytul, biblWypoz from biblAutor_biblTytul,biblAutor,biblTytul where biblAutor.id=biblAutor_id and biblTytul.id=biblTytul_id";
-if ($conn->connect_error) {
-die("Connection failed: " . $conn->connect_error);
-}
-$result = mysqli_query($conn, $sql);
-if ( $result) {
-} else {
-echo "Error: " . $sql . "<br>" . mysqli_error($conn);
-}
+$sql = "select (`biblAutor_biblTytul`.id) as ID_TAB, autor, tytul, biblWypoz from biblAutor_biblTytul,biblAutor,biblTytul where biblAutor.id=biblAutor_id and biblTytul.id=biblTytul_id order by autor,ID_TAB asc";
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+      }
+    $result = mysqli_query($conn, $sql);
+    if ( $result) {
+     } else {
+       echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+     }
 echo("<table border='1' class='tabela'>");
 while($row = mysqli_fetch_assoc($result)) {
 echo("<tr>");
