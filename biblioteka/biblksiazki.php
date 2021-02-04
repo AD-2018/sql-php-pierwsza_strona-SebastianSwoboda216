b0620954805c6e85a5568202eba5086102a83005
SebastianSwoboda216 <sebastian.swoboda83@gmail.com>
2021-02-04 19:27:19 +0100

    cvc

--- biblioteka/biblksiazki.php
+++ biblioteka/biblksiazki.php
@@ -26,6 +26,7 @@
 <div>
 <?php 
 require_once("../connect.php"); 
+echo("<h1>Rozwijana lista</h1>"); 
 $sql = "select (biblAutor_biblTytul.id) as ID_TAB, autor, tytul, biblWypoz from biblAutor_biblTytul,biblAutor,biblTytul where biblAutor.id=biblAutor_id and biblTytul.id=biblTytul_id order by autor,ID_TAB asc";
 $result = mysqli_query($conn, $sql);
 if ( $result) {
