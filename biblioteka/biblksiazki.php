04d039e2006b8a5e177f682fafdb4ef22423cd39
SebastianSwoboda216 <sebastian.swoboda83@gmail.com>
2021-02-04 13:47:11 +0100

    commit

--- biblioteka/biblksiazki.php
+++ biblioteka/biblksiazki.php
@@ -4,22 +4,50 @@
 <link rel="stylesheet" href="../style.css">
 </head>
 <body>
-<?php   
+<div class="h1">
+<a href="../index.php">Menu</a>
+</div>
+<?php 
 require_once("connect.php");
-$sql = "select (`biblAutor_biblTytul`.id), autor, tytul, biblWypoz from biblAutor_biblTytul,biblAutor,biblTytul where biblAutor.id=biblAutor_id and biblTytul.id=biblTytul_id";
-if ($conn->connect_error) {
-die("Connection failed: " . $conn->connect_error);
-}
+$sql = "select (biblTytul_biblAutor.id) as ID_TAB, autor, tytul, biblWypoz from biblTytul_biblAutor,biblAutor,biblTytul where biblAutor.id=biblAutor_id and biblTytul.id=biblTytul_id order by autor,ID_TAB asc";
 $result = mysqli_query($conn, $sql);
 if ( $result) {
 } else {
 echo "Error: " . $sql . "<br>" . mysqli_error($conn);
 }
-echo("<table border='1' class='tabela'>");
+echo('<select name="ksiazka">');
 while($row = mysqli_fetch_assoc($result)) {
-echo("<tr>");
-echo("<td>".$row['biblWypoz']."</td><td>".$row['autor']."</td><td>".$row['tytul']."</td>");  
-?>
-</body>
-</html>
+echo '<option value="'.$row['id'].'">';
+echo($row['autor'].', '.$row['tytul']);
+echo "</option>";
+};
+echo('</select>');
+echo ('<br>');
+echo("<h1>Tytuły</h1>"); 
+$sql = "select * from biblTytul";
+echo("<li>".$sql);
+$result = mysqli_query($conn, $sql);
+echo('<table border="1" class="tabela"'); 
+echo ("<tr><th>id</th><th>tytul</th></tr>");
+while ($row = mysqli_fetch_assoc($result)) {
+echo("<tr>");     
+echo("<tr>");   
+echo ('<td>'.$row["id"].'</td><td>'.$row["tytul"].'</td>');
+echo("<tr>"); 
+echo("</tr>"); } 
+echo('</table>');	
+echo("<h1>Autorzy</h1>"); 
+$sql = "select * from biblAutor";
+echo("<li>".$sql);
+$result = mysqli_query($conn, $sql);
+echo('<table border="1" class="tabela"'); 
+echo ("<tr><th>id</th><th>autor</th></tr>");
+while ($row = mysqli_fetch_assoc($result)) {
+echo("<tr>");     
+echo("<tr>");   
+echo ('<td>'.$row["id"].'</td><td>'.$row["autor"].'</td>');
+echo("<tr>"); 
+echo("</tr>"); } 
+echo('</table>'); 
+?> 
     
