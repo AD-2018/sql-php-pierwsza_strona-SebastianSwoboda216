788313573118083cbd57f0900e6f1d3bae275520
Sebastian Swoboda <73635276+SebastianSwoboda216@users.noreply.github.com>
2021-04-15 14:08:55 +0200

    Update grid1.php

--- grid/grid1.php
+++ grid/grid1.php
@@ -1,19 +1,1 @@
-<!DOCTYPE html>
-<html>
-<head>
-<title>Grid 1</title>
-<meta charset="UTF-8">
-<link rel="stylesheet" href="Grid.css">
-</head>
-<body>
-<strong>
-<div class="str1">
-<div class="str1A">
-<?php
-require_once("../connect.php");
-$sql = "SELECT * FROM biblTytul";
-if ($conn->connect_error) {
-die("Connection failed: " . $conn->connect_error);
-}
-$result = mysqli_query($conn, $sql);
 
