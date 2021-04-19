6d4a1495c89e351280d82c12bc9c7ea7e51d3d73
Sebastian Swoboda <73635276+SebastianSwoboda216@users.noreply.github.com>
2021-04-19 08:44:04 +0200

    Update form.php

--- pracownicy/form.php
+++ pracownicy/form.php
@@ -2,10 +2,11 @@
 <head> Formularz </head>
 <title>Sebastian Swoboda 3 Ti</title>
 <body>
+<?php include"../menu.php" ?>
 <form action="strona.php" method="POST">
    <input type="text" name="firstname"></br>
    <input type="submit" value="wyślij do strona.php">
 </form>
-   <a href="index.php">menu</a>
+ 
 </body>
 </html>
