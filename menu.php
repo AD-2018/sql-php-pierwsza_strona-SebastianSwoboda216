491d661add277cc88afde3f4cbbf9406f77cdad2
Sebastian Swoboda <73635276+SebastianSwoboda216@users.noreply.github.com>
2021-04-19 08:19:40 +0200

    Create menu.php

---
+++ menu.php
@@ -0,0 +1,17 @@
+<nav>
+<div class="nav">
+<strong>
+<li><a href="https://github.com/AD-2018/sql-php-pierwsza_strona-SebastianSwoboda216">github</a></li>
+<li><a href="pracownicy/pracownicyorg.php">Pracownicy Organizacja.</a></li>
+<li><a href="pracownicy/fagregujace.php">Funkcje Agregujące</a></li>
+<li><a href="pracownicy/pracownicy.php">Pracownicy</a></li>
+<li><a href="pracownicy/dataczas.php">Data i czas</a></li>
+<li><a href="pracownicy/form.php">Formularz</a></li>
+<li><a href="pracownicy/form2.php">Formularz osobowy</a></li>
+<li><a href="pracownicy/danedobazy.php">Usuwanie dodawanie</a></li>
+<li><a href="biblioteka/biblksiazki.php">Biblioteka Ksiazki</a></li>
+<li><a href="grid/index.php">Grid</a></li>
+</strong>
+</div>
+</br></br></br>
+</nav>
