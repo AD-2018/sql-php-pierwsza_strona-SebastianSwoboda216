f41e1cc03b5ec340396205e52be08a3227298353
SebastianSwoboda216 <sebastian.swoboda83@gmail.com>
2021-04-21 17:30:46 +0200

    delete

--- grid/grid2.php
+++ grid/grid2.php
@@ -1,92 +1,150 @@
 <!DOCTYPE html>
 <html>
-<head>
-<title>Grid 2</title>
-<meta charset="UTF-8">
-<link rel="stylesheet" href="Grid.css">
-</head>
-<body>
-<strong>
-<div class="str2">
-<div class="str2A">
-<?php
-require_once("../connect.php");
-$sql = "select Klasa, `Imie-Nazwisko`, (`WDW`.ID) as ID_TAB from `aaaaaaa_tojestbazadanych`.WDW, `aaaaaaa_tojestbazadanych`.Klasa, `aaaaaaa_tojestbazadanych`.Osoby where Osoby.ID=osoby_id and Klasa.ID=klasa_id order by ID_TAB asc";
-if ($conn->connect_error) {
-die("Connection failed: " . $conn->connect_error);
-}
-$result = mysqli_query($conn, $sql);
-if ( $result) {
-} else {
-echo "Error: " . $sql . "<br>" . mysqli_error($conn);
-}
-echo("<h1>Wiele do Wielu</h1>");
-echo("<table border='1'>");
-echo("<th>ID</th><th>Nauczyciel</th><th>Klasa</th>");
-while($row = mysqli_fetch_assoc($result)) {
-echo("<tr>");
-echo("<td>".$row['ID_TAB']."</td><td>".$row['Imie-Nazwisko']."</td><td>".$row['Klasa']."</td>");
-echo("</tr>");
-};
-echo("</table>");
-echo ("<br>");
-?>
-</div>
-<div class="str2B">
-<?php
-require_once("../connect.php");
-$sql = "SELECT * FROM Osoby";
-if ($conn->connect_error) {
-die("Connection failed: " . $conn->connect_error);
-}
-$result = mysqli_query($conn, $sql);
-if ( $result) {
-echo "<br>";
-} else {
-echo "Error: " . $sql . "<br>" . mysqli_error($conn);
-}
-echo("<h1>Nauczyciele</h1>");
-echo("<table border='1'>");
-echo("<th>ID</th><th>Imię i Nazwisko</th>");
-while($row = mysqli_fetch_assoc($result)) {
-echo("<tr>");
-echo("<td>".$row['ID']."</td><td>".$row['Imie-Nazwisko']."</td>");
-echo("</tr>");
-};
-echo("</table>");
-echo ("<br>");
-?>
-</div>
-<div class="str2C">
-<?php
-require_once("../connect.php");
-$sql = "SELECT * FROM Klasa";
-if ($conn->connect_error) {
-die("Connection failed: " . $conn->connect_error);
-}
-$result = mysqli_query($conn, $sql);
-if ( $result) {
-echo "<br>";
-} else {
-echo "Error: " . $sql . "<br>" . mysqli_error($conn);
-}
-echo("<h1>Klasa</h1>");
-echo("<table border='1'>");
-echo("<th>ID</th><th>Klasa</th>");
-while($row = mysqli_fetch_assoc($result)) {
-echo("<tr>");
-echo("<td>".$row['ID']."</td><td>".$row['Klasa']."</td>");
-echo("</tr>");
-};
-echo("</table>");
-echo ("<br>");
-?>
-</div>
-<div class="str2D">4</div>
-<div class="str2E">5</div>
-<div class="str2F">Szkoła</div>
-</div>
-</strong>
-</body>
-</html>
 
+<head>
+    <title>Grid 2</title>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="Grid.css">
+</head>
+
+<body>
+    <strong>
+    <div class="str2">
+        <div class="str2E">
+        <?php
+                        require_once("../connect.php");
+                        $sql = "select Klasa, `Imie-Nazwisko`, (`WDW`.ID) as ID_TAB from `aaaaaaa_tojestbazadanych`.WDW, `aaaaaaa_tojestbazadanych`.Klasa, `aaaaaaa_tojestbazadanych`.Osoby where Osoby.ID=osoby_id and Klasa.ID=klasa_id order by ID_TAB asc";
+                            if ($conn->connect_error) {
+                                    die("Connection failed: " . $conn->connect_error);
+                             }
+                                $result = mysqli_query($conn, $sql);
+                            if ( $result) {
+                            } else {
+                                echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+                            }
+                        
+                            echo("<h1>Wiele do Wielu</h1>");
+                        
+                            echo("<table border='1'>");
+                            echo("<th>ID</th><th>Nauczyciel</th><th>Klasa</th>");
+                                while($row = mysqli_fetch_assoc($result)) {
+                                    echo("<tr>");
+                                    echo("<td>".$row['ID_TAB']."</td><td>".$row['Imie-Nazwisko']."</td><td>".$row['Klasa']."</td>".'<td>
+	    
+                                    <form action="delete.php" method="POST">
+                                     <input name="id" value="'.$row['ID_TAB'].'" hidden>
+                                     <input name="tabela" value="WDW" hidden>
+                                      <input type="submit" class="button_x" value="X">
+                                    </form>
+                                    
+                                    </td>');
+                                   echo('</tr>');
+                               }
+                            echo("</table>");
+                            echo ("<br>");
+                ?>
+                <h3>Usuwanie po ID</h3>
+                <form action="delete.php" method="POST">
+                    <input type="number" name="id">
+                    <input name="tabela" value="WDW" hidden>
+                    <input type="submit" class="button_x" value="USUŃ">
+                </form>
+        </div>
+        <div class="str2F">
+        <?php
+                require_once("../connect.php");
+                $sql = "SELECT * FROM Osoby";
+                
+                    if ($conn->connect_error) {
+                        die("Connection failed: " . $conn->connect_error);
+                      }
+                
+                    $result = mysqli_query($conn, $sql);
+                    if ( $result) {
+                         echo "<br>";
+                     } else {
+                       echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+                     }
+                
+                    echo("<h1>Nauczyciele</h1>");
+                
+                    echo("<table border='1'>");
+                    echo("<th>ID</th><th>Imię i Nazwisko</th>");
+                        while($row = mysqli_fetch_assoc($result)) {
+                            echo("<tr>");
+                            echo("<td>".$row['ID']."</td><td>".$row['Imie-Nazwisko']."</td>".'<td>
+	    
+                            <form action="delete.php" method="POST">
+                             <input name="id" value="'.$row['ID'].'" hidden>
+                             <input name="tabela" value="Osoby" hidden>
+                             <input name="opcja" value="1" hidden>
+                              <input type="submit" class="button_x" value="X">
+                            </form>
+                            
+                            </td>');
+                           echo('</tr>');
+                       }
+                    echo("</table>");
+                    echo ("<br>");
+            ?>
+                            <h3>Usuwanie po ID</h3>
+                <form action="delete.php" method="POST">
+                    <input type="number" name="id">
+                    <input name="tabela" value="Osoby" hidden>
+                             <input name="opcja" value="1" hidden>
+                    <input type="submit" class="button_x" value="USUŃ">
+                </form>
+        </div>
+        <div class="str2C">
+        <?php
+                require_once("../connect.php");
+                $sql = "SELECT * FROM Klasa";
+                
+                    if ($conn->connect_error) {
+                        die("Connection failed: " . $conn->connect_error);
+                      }
+                
+                    $result = mysqli_query($conn, $sql);
+                    if ( $result) {
+                         echo "<br>";
+                     } else {
+                       echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+                     }
+                
+                    echo("<h1>Klasa</h1>");
+                
+                    echo("<table border='1'>");
+                    echo("<th>ID</th><th>Klasa</th>");
+                        while($row = mysqli_fetch_assoc($result)) {
+                            echo("<tr>");
+                            echo("<td>".$row['ID']."</td><td>".$row['Klasa']."</td>".'<td>
+	    
+                            <form action="delete.php" method="POST">
+                             <input name="id" value="'.$row['ID'].'" hidden>
+                             <input name="tabela" value="Klasa" hidden>
+                             <input name="opcja" value="2" hidden>
+                              <input type="submit" class="button_x" value="X">
+                            </form>
+                            
+                            </td>');
+                           echo('</tr>');
+                       }
+                    echo("</table>");
+                    echo ("<br>");
+            ?>
+                                        <h3>Usuwanie po ID</h3>
+                <form action="delete.php" method="POST">
+                    <input type="number" name="id">
+                    <input name="tabela" value="Klasa" hidden>
+                             <input name="opcja" value="2" hidden>
+                    <input type="submit" class="button_x" value="USUŃ">
+                </form>
+        </div>
+        <div class="str2D">4</div>
+        <div class="str2A">5</div>
+        <div class="str2B">Szkoła</div>
+    </div>
+    </strong>
+</body>
+
+</html>
--- grid/grid3.php
+++ grid/grid3.php
@@ -1,88 +1,148 @@
 <!DOCTYPE html>
 <html>
+
 <head>
-<title>Grid 3</title>
-<meta charset="UTF-8">
-<link rel="stylesheet" href="Grid.css">
+    <title>Grid 3</title>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="Grid.css">
 </head>
+
 <body>
-<strong>
-<div class="str3">
-<div class="str1A">
-<?php
-require_once("../connect.php");
-$sql = "SELECT * FROM osoby_v2";
-if ($conn->connect_error) {
-die("Connection failed: " . $conn->connect_error);
-}
-$result = mysqli_query($conn, $sql);
-if ( $result) {
-echo "<br>";
-} else {
-echo "Error: " . $sql . "<br>" . mysqli_error($conn);
-}
-echo("<h1>Lekarze</h1>");
-echo("<table border='1'>");
-echo("<th>ID</th><th>Lekarz</th>");
-while($row = mysqli_fetch_assoc($result)) {
-echo("<tr>");
-echo("<td>".$row['id']."</td><td>".$row['imiona']."</td>");
-echo("</tr>");
-};
-echo("</table>");
-echo ("<br>");
-?>
-</div>
-<div class="str1B"><?php
-require_once("../connect.php");
-$sql = "SELECT * FROM Osoby";
-if ($conn->connect_error) {
-die("Connection failed: " . $conn->connect_error);
-}
-$result = mysqli_query($conn, $sql);
-if ( $result) {
-echo "<br>";
-} else {
-echo "Error: " . $sql . "<br>" . mysqli_error($conn);
-}
-echo("<h1>Pacjenci</h1>");
-echo("<table border='1'>");
-echo("<th>ID</th><th>Imię i Nazwisko</th>");
-while($row = mysqli_fetch_assoc($result)) {
-echo("<tr>");
-echo("<td>".$row['ID']."</td><td>".$row['Imie-Nazwisko']."</td>");
-echo("</tr>");
-};
-echo("</table>");
-echo ("<br>");
-?></div>
-<div class="str1C">
-<?php
-require_once("../connect.php");
-$sql = "select imiona, `Imie-Nazwisko`, (`WDW`.ID) as ID_TAB from `aaaaaaa_tojestbazadanych`.WDW, `aaaaaaa_tojestbazadanych`.osoby_v2, `aaaaaaa_tojestbazadanych`.Osoby where Osoby.ID=osoby_id and osoby_v2.id=klasa_id order by ID_TAB asc";
-if ($conn->connect_error) {
-die("Connection failed: " . $conn->connect_error);
-}
-$result = mysqli_query($conn, $sql);
-if ( $result) {
-} else {
-echo "Error: " . $sql . "<br>" . mysqli_error($conn);
-}
-echo("<h1>Wiele do Wielu</h1>");
-echo("<table border='1'>");
-echo("<th>ID</th><th>Lekarz</th><th>Pacjent</th>");
-while($row = mysqli_fetch_assoc($result)) {
-echo("<tr>");
-echo("<td>".$row['ID_TAB']."</td><td>".$row['imiona']."</td><td>".$row['Imie-Nazwisko']."</td>");
-echo("</tr>");
-};
-echo("</table>");
-echo ("<br>");
-?>
-</div>
-<div class="str1D">4</div>
-<div class="str1E">Przychodnia</div>
-</div>
-</strong>
+    <strong>
+    <div class="str3">
+        <div class="str1A">
+        <?php
+                require_once("../connect.php");
+                $sql = "SELECT * FROM osoby_v2";
+                
+                    if ($conn->connect_error) {
+                        die("Connection failed: " . $conn->connect_error);
+                      }
+                
+                    $result = mysqli_query($conn, $sql);
+                    if ( $result) {
+                         echo "<br>";
+                     } else {
+                       echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+                     }
+                
+                    echo("<h1>Lekarze</h1>");
+                
+                    echo("<table border='1'>");
+                    echo("<th>ID</th><th>Lekarz</th>");
+                        while($row = mysqli_fetch_assoc($result)) {
+                            echo("<tr>");
+                            echo("<td>".$row['id']."</td><td>".$row['imiona']."</td>".'<td>
+	    
+                            <form action="delete.php" method="POST">
+                             <input name="id" value="'.$row['id'].'" hidden>
+                             <input name="tabela" value="osoby_v2" hidden>
+                             <input name="opcja" value="1" hidden>
+                              <input type="submit" class="button_x" value="X">
+                            </form>
+                            
+                            </td>');
+                           echo('</tr>');
+                       }
+                    echo("</table>");
+                    echo ("<br>");
+                    ?>
+                                                            <h3>Usuwanie po ID</h3>
+                <form action="delete.php" method="POST">
+                    <input type="number" name="id">
+                    <input name="tabela" value="osoby_v2" hidden>
+                             <input name="opcja" value="1" hidden>
+                    <input type="submit" class="button_x" value="USUŃ">
+                </form>
+        </div>
+        <div class="str1B"><?php
+                require_once("../connect.php");
+                $sql = "SELECT * FROM Osoby";
+                
+                    if ($conn->connect_error) {
+                        die("Connection failed: " . $conn->connect_error);
+                      }
+                
+                    $result = mysqli_query($conn, $sql);
+                    if ( $result) {
+                         echo "<br>";
+                     } else {
+                       echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+                     }
+                
+                    echo("<h1>Pacjenci</h1>");
+                
+                    echo("<table border='1'>");
+                    echo("<th>ID</th><th>Imię i Nazwisko</th>");
+                        while($row = mysqli_fetch_assoc($result)) {
+                            echo("<tr>");
+                            echo("<td>".$row['ID']."</td><td>".$row['Imie-Nazwisko']."</td>".'<td>
+	    
+                            <form action="delete.php" method="POST">
+                             <input name="id" value="'.$row['ID'].'" hidden>
+                             <input name="tabela" value="Osoby" hidden>
+                             <input name="opcja" value="2" hidden>
+                              <input type="submit" class="button_x" value="X">
+                            </form>
+                            
+                            </td>');
+                           echo('</tr>');
+                       }
+                    echo("</table>");
+                    echo ("<br>");
+            ?>
+                                                                        <h3>Usuwanie po ID</h3>
+                <form action="delete.php" method="POST">
+                    <input type="number" name="id">
+                    <input name="tabela" value="Osoby" hidden>
+                             <input name="opcja" value="2" hidden>
+                    <input type="submit" class="button_x" value="USUŃ">
+                </form>
+            </div>
+        <div class="str1C">
+        <?php
+                        require_once("../connect.php");
+                        $sql = "select imiona, `Imie-Nazwisko`, (`WDW`.ID) as ID_TAB from `aaaaaaa_tojestbazadanych`.WDW, `aaaaaaa_tojestbazadanych`.osoby_v2, `aaaaaaa_tojestbazadanych`.Osoby where Osoby.ID=osoby_id and osoby_v2.id=klasa_id order by ID_TAB asc";
+                            if ($conn->connect_error) {
+                                    die("Connection failed: " . $conn->connect_error);
+                             }
+                                $result = mysqli_query($conn, $sql);
+                            if ( $result) {
+                            } else {
+                                echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+                            }
+                        
+                            echo("<h1>Wiele do Wielu</h1>");
+                        
+                            echo("<table border='1'>");
+                            echo("<th>ID</th><th>Lekarz</th><th>Pacjent</th>");
+                                while($row = mysqli_fetch_assoc($result)) {
+                                    echo("<tr>");
+                                    echo("<td>".$row['ID_TAB']."</td><td>".$row['imiona']."</td><td>".$row['Imie-Nazwisko']."</td>".'<td>
+	    
+                                    <form action="delete.php" method="POST">
+                                     <input name="id" value="'.$row['ID_TAB'].'" hidden>
+                                     <input name="tabela" value="WDW" hidden>
+                                      <input type="submit" class="button_x" value="X">
+                                    </form>
+                                    
+                                    </td>');
+                                   echo('</tr>');
+                               }
+                            echo("</table>");
+                            echo ("<br>");
+                ?>
+                                <h3>Usuwanie po ID</h3>
+                <form action="delete.php" method="POST">
+                    <input type="number" name="id">
+                    <input name="tabela" value="WDW" hidden>
+                    <input type="submit" class="button_x" value="USUŃ">
+                </form>
+        </div>
+        <div class="str1D">4</div>
+        <div class="str1E">Przychodnia</div>
+    </div>
+    </strong>
 </body>
+
 </html>
